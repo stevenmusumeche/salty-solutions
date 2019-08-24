9dd7ad629d26ca7f74547552668a857a5f056aa7
Steven Musumeche <steven.musumeche@external.milliporesigma.com>
2019-08-24 15:07:51 -0500

    add purgecss optimization

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -1,5 +1,16 @@
+const Purgecss = require("@fullhuman/postcss-purgecss");
 var tailwindcss = require("tailwindcss");
 
+// see https://tailwindcss.com/docs/controlling-file-size
+const purgeCss = new Purgecss({
+  content: ["**/*.tsx", "**/*.html"],
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+});
+
 module.exports = {
-  plugins: [tailwindcss("./tailwind.config.js"), require("autoprefixer")]
+  plugins: [
+    tailwindcss("./tailwind.config.js"),
+    require("autoprefixer"),
+    ...(process.env.NODE_ENV === "production" ? [purgeCss] : [])
+  ]
 };
