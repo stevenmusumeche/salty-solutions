da6e8f28b211d915d8c9a538ecdfcfc0fa29ac04
Steven Musumeche <steven@musumeche.com>
2019-11-07 19:26:18 -0600

    add bugsnag with test error for source maps

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect } from "react";
 import ReactDOM from "react-dom";
 import "whatwg-fetch";
+import bugsnag from "@bugsnag/js";
+import bugsnagReact from "@bugsnag/plugin-react";
 
 import App from "./App";
 import { Provider as UrqlProvider, createClient } from "urql";
@@ -8,6 +10,12 @@ import { Router, Redirect } from "@reach/router";
 import About from "./components/About";
 import useLocalStorage from "./hooks/useLocalStorage";
 import MediaQueryProvider from "./providers/WindowSizeProvider";
+
+const bugsnagClient = bugsnag("c1260c2450f57d001639a05b6c1335f7");
+bugsnagClient.use(bugsnagReact, React);
+const ErrorBoundary = bugsnagClient.getPlugin("react");
+
+bugsnagClient.notify(new Error("Test error"));
 
 export const INITIAL_LOCATION = "calcasieu-lake";
 
@@ -26,15 +34,17 @@ const Root = () => {
   }, []);
 
   return (
-    <MediaQueryProvider>
-      <UrqlProvider value={client}>
-        <Router>
-          <About path="/about" />
-          <App path="/:locationSlug" />
-          <Redirect from="/" to={`/${locationId}`} noThrow />
-        </Router>
-      </UrqlProvider>
-    </MediaQueryProvider>
+    <ErrorBoundary>
+      <MediaQueryProvider>
+        <UrqlProvider value={client}>
+          <Router>
+            <About path="/about" />
+            <App path="/:locationSlug" />
+            <Redirect from="/" to={`/${locationId}`} noThrow />
+          </Router>
+        </UrqlProvider>
+      </MediaQueryProvider>
+    </ErrorBoundary>
   );
 };
 
