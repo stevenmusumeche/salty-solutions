2b73fd837cd63dae83ff586b0e9f733c2728b56a
Steven Musumeche <steven@musumeche.com>
2019-12-02 17:57:23 -0600

    replace FE bug tracking with rollbar

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,8 +1,6 @@
 import React, { useEffect } from "react";
 import ReactDOM from "react-dom";
 import "whatwg-fetch";
-import bugsnag from "@bugsnag/js";
-import bugsnagReact from "@bugsnag/plugin-react";
 
 import App from "./App";
 import { Provider as UrqlProvider, createClient } from "urql";
@@ -10,10 +8,6 @@ import { Router, Redirect } from "@reach/router";
 import About from "./components/About";
 import useLocalStorage from "./hooks/useLocalStorage";
 import MediaQueryProvider from "./providers/WindowSizeProvider";
-
-const bugsnagClient = bugsnag("c1260c2450f57d001639a05b6c1335f7");
-bugsnagClient.use(bugsnagReact, React);
-const ErrorBoundary = bugsnagClient.getPlugin("react");
 
 export const INITIAL_LOCATION = "calcasieu-lake";
 
@@ -32,17 +26,15 @@ const Root = () => {
   }, []);
 
   return (
-    <ErrorBoundary>
-      <MediaQueryProvider>
-        <UrqlProvider value={client}>
-          <Router>
-            <About path="/about" />
-            <App path="/:locationSlug" />
-            <Redirect from="/" to={`/${locationId}`} noThrow />
-          </Router>
-        </UrqlProvider>
-      </MediaQueryProvider>
-    </ErrorBoundary>
+    <MediaQueryProvider>
+      <UrqlProvider value={client}>
+        <Router>
+          <About path="/about" />
+          <App path="/:locationSlug" />
+          <Redirect from="/" to={`/${locationId}`} noThrow />
+        </Router>
+      </UrqlProvider>
+    </MediaQueryProvider>
   );
 };
 
