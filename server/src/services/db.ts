ce3b67666beeef612bc19b7eb8816a0470283a61
Steven Musumeche <steven@musumeche.com>
2020-03-12 14:13:25 -0500

    ok

--- server/src/services/db.ts
+++ server/src/services/db.ts
@@ -9,22 +9,28 @@ export const getCacheVal = async <T extends MapAttributeValue>(
   key: string,
   maxAgeMins: number
 ): Promise<T | void> => {
-  const cutoff = subMinutes(new Date(), maxAgeMins);
-  const result = await client
-    .query({
-      TableName: tableName,
-      KeyConditionExpression: "pk = :key AND sk >= :date",
-      ExpressionAttributeValues: {
-        ":key": key,
-        ":date": cutoff.getTime()
-      },
-      Limit: 1,
-      ScanIndexForward: false
-    })
-    .promise();
-  if (result.Count === 0) return;
+  try {
+    const cutoff = subMinutes(new Date(), maxAgeMins);
+    const result = await client
+      .query({
+        TableName: tableName,
+        KeyConditionExpression: "pk = :key AND sk >= :date",
+        ExpressionAttributeValues: {
+          ":key": key,
+          ":date": cutoff.getTime()
+        },
+        Limit: 1,
+        ScanIndexForward: false
+      })
+      .promise();
+    if (result.Count === 0) return;
 
-  return result.Items![0] as T;
+    return result.Items![0] as T;
+  } catch (e) {
+    console.log("Error fetching cached value", e);
+
+    return;
+  }
 };
 
 export const setCacheVal = async <T extends MapAttributeValue>(
