810ef18bc231857366d284a75d804422295c794a
Steven Musumeche <steven@musumeche.com>
2020-03-12 14:43:07 -0500

    try again

--- server/src/preloaders.ts
+++ server/src/preloaders.ts
@@ -2,9 +2,11 @@ import { ScheduledHandler } from "aws-lambda";
 import { getAll } from "./services/location";
 import { getForecast } from "./services/marine";
 
-export const forecast: ScheduledHandler = async event => {
+export const forecast: ScheduledHandler = async (event, ctx, cb) => {
   for (const location of getAll()) {
     console.log("Preloading forecast for " + location.name);
     await getForecast(location);
   }
+
+  cb(null);
 };
