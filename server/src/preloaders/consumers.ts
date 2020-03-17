e8f59c784f7cc4e32fcbb84e232272d342d13310
Steven Musumeche <steven@musumeche.com>
2020-03-17 10:05:11 -0500

    fix consumer

--- server/src/preloaders/consumers.ts
+++ server/src/preloaders/consumers.ts
@@ -4,11 +4,12 @@ import { getForecast } from "../services/weather";
 import { getById } from "../services/location";
 
 export const forecast: SQSHandler = async (event, ctx, cb) => {
-  event.Records.forEach(async record => {
+  for (const record of event.Records) {
     const payload = JSON.parse(record.body);
     console.log("Processing forecast for ", payload.locationId);
     const location = getById(payload.locationId);
     if (!location) throw new Error(`Unknown location ${payload.locationId}`);
     await Promise.all([getMarineForecast(location), getForecast(location)]);
-  });
+    console.log("Finished processing forecast for ", payload.locationId);
+  }
 };
