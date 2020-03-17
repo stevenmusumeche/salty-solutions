4764d03f45952f3198b3c51c75098150205867a2
Steven Musumeche <steven@musumeche.com>
2020-03-17 10:24:37 -0500

    adjust perms

--- server/src/preloaders/consumers.ts
+++ server/src/preloaders/consumers.ts
@@ -6,10 +6,10 @@ import { getById } from "../services/location";
 export const forecast: SQSHandler = async (event, ctx, cb) => {
   for (const record of event.Records) {
     const payload = JSON.parse(record.body);
-    console.log("Processing forecast for ", payload.locationId);
+    console.log("Preloading forecast for", payload.locationId);
+
     const location = getById(payload.locationId);
     if (!location) throw new Error(`Unknown location ${payload.locationId}`);
     await Promise.all([getMarineForecast(location), getForecast(location)]);
-    console.log("Finished processing forecast for ", payload.locationId);
   }
 };
