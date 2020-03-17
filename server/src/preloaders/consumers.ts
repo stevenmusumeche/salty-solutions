b20932fad34f0468f4ea09fca0814aa3eab5d084
Steven Musumeche <steven@musumeche.com>
2020-03-16 19:54:59 -0500

    add consumer

--- server/src/preloaders/consumers.ts
+++ server/src/preloaders/consumers.ts
@@ -1,5 +1,15 @@
 import { SQSHandler } from "aws-lambda";
 
 export const forecast: SQSHandler = async (event, ctx, cb) => {
-  console.log("received SQS forecast event", event.Records);
+  console.log(
+    "received sqs event with this many records",
+    event.Records.length
+  );
+
+  event.Records.forEach(record => {
+    const payload = JSON.parse(record.body);
+    console.log("received SQS forecast event", payload);
+  });
+
+  // cb("test error");
 };
