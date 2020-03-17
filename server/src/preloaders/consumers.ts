a18e2f69e3158e3e63f72ba7b5806ba42fcb2a57
Steven Musumeche <steven@musumeche.com>
2020-03-17 11:04:55 -0500

    remove test error

--- server/src/preloaders/consumers.ts
+++ server/src/preloaders/consumers.ts
@@ -7,11 +7,8 @@ export const forecast: SQSHandler = async (event, ctx, cb) => {
   for (const record of event.Records) {
     const payload = JSON.parse(record.body);
     console.log("Preloading forecast for", payload.locationId);
-
-    throw new Error("test error " + payload.locationId);
-
-    // const location = getById(payload.locationId);
-    // if (!location) throw new Error(`Unknown location ${payload.locationId}`);
-    // await Promise.all([getMarineForecast(location), getForecast(location)]);
+    const location = getById(payload.locationId);
+    if (!location) throw new Error(`Unknown location ${payload.locationId}`);
+    await Promise.all([getMarineForecast(location), getForecast(location)]);
   }
 };
