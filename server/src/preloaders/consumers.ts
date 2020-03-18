be8a896e8d49c100220443c95753aaeedbec34eb
Steven Musumeche <steven@musumeche.com>
2020-03-18 18:31:24 -0500

    add tide preloader

--- server/src/preloaders/consumers.ts
+++ server/src/preloaders/consumers.ts
@@ -2,6 +2,7 @@ import { SQSHandler } from "aws-lambda";
 import { getForecast as getMarineForecast } from "../services/marine";
 import { getForecast } from "../services/weather";
 import { getById } from "../services/location";
+import { getTidePredictions } from "../services/tide";
 
 export const forecast: SQSHandler = async (event, ctx, cb) => {
   for (const record of event.Records) {
@@ -10,5 +11,23 @@ export const forecast: SQSHandler = async (event, ctx, cb) => {
     const location = getById(payload.locationId);
     if (!location) throw new Error(`Unknown location ${payload.locationId}`);
     await Promise.all([getMarineForecast(location), getForecast(location)]);
+  }
+};
+
+export const tide: SQSHandler = async (event, ctx, cb) => {
+  for (const record of event.Records) {
+    const payload = JSON.parse(record.body);
+    console.log(
+      "Preloading tides for",
+      payload.stationId,
+      payload.startDate,
+      payload.endDate
+    );
+
+    await getTidePredictions(
+      new Date(payload.startDate),
+      new Date(payload.endDate),
+      payload.stationId
+    );
   }
 };
--- server/src/services/queue.ts
+++ server/src/services/queue.ts
@@ -20,3 +20,25 @@ export const sendMessage = (
 
   return sqs.sendMessage(params).promise();
 };
+
+export const sendMessageBatch = (
+  queueUrl: string,
+  producerName: string,
+  bodies: Object[]
+) => {
+  return sqs
+    .sendMessageBatch({
+      QueueUrl: queueUrl,
+      Entries: bodies.map((body, i) => ({
+        Id: String(i),
+        MessageAttributes: {
+          producer: {
+            DataType: "String",
+            StringValue: producerName
+          }
+        },
+        MessageBody: JSON.stringify(body)
+      }))
+    })
+    .promise();
+};
