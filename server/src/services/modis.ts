7a3943595687d5fb2b3137b71ff2a78fcaec51b7
Steven Musumeche <steven@musumeche.com>
2020-05-10 09:55:00 -0500

    add caching to modis

--- server/src/services/modis.ts
+++ server/src/services/modis.ts
@@ -3,13 +3,18 @@ import { LocationEntity } from "./location";
 import axios from "axios";
 import axiosRetry from "axios-retry";
 import { ModusMap } from "../../../client/src/generated/graphql";
+import { getCacheVal, setCacheVal } from "./db";
 
-axiosRetry(axios, { retries: 3, retryDelay: retryCount => retryCount * 500 });
+axiosRetry(axios, { retries: 3, retryDelay: (retryCount) => retryCount * 500 });
 
 export const getMaps = async (
   location: LocationEntity,
   numDays: number
 ): Promise<ModusMap[]> => {
+  const cacheKey = `modis-${location.modisArea}-${numDays}`;
+  const cachedData = await getCacheVal<ModusMap[]>(cacheKey, 3 * 60 * 4); // fresh for 4 hours
+  if (cachedData) return cachedData;
+
   let result: ModusMap[] = [];
   for (let i = 0; i < numDays; i++) {
     const date = subDays(new Date(), i);
@@ -19,24 +24,24 @@ export const getMaps = async (
       small: {
         url: baseUrl.replace("[SIZE]", "2000m"),
         width: 650,
-        height: 750
+        height: 750,
       },
       medium: {
         url: baseUrl.replace("[SIZE]", "1000m"),
         width: 1300,
-        height: 1500
+        height: 1500,
       },
       large: {
         url: baseUrl.replace("[SIZE]", "250m"),
         width: 5200,
-        height: 6000
-      }
+        height: 6000,
+      },
     });
   }
 
   result = await removeBrokenImages(result);
 
-  return result.sort((a: ModusMap, b: ModusMap) => {
+  const sorted = result.sort((a: ModusMap, b: ModusMap) => {
     const aDate = new Date(a.date);
     const bDate = new Date(b.date);
 
@@ -45,6 +50,8 @@ export const getMaps = async (
 
     return 0;
   });
+
+  return setCacheVal(cacheKey, sorted);
 };
 
 function buildBaseUrl(date: Date, location: LocationEntity) {
@@ -62,7 +69,7 @@ function buildBaseUrl(date: Date, location: LocationEntity) {
 
 async function removeBrokenImages(result: ModusMap[]): Promise<ModusMap[]> {
   const promises = await Promise.all(
-    result.map(async val => {
+    result.map(async (val) => {
       const imageUrl = val.small.url;
       let valid = false;
       await axios
