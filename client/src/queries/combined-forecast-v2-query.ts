72006c50e7e12f024dadb1ca646ba5d2361aea61
Steven Musumeche <steven@musumeche.com>
2020-05-31 19:31:28 -0500

    v1.0.29

---
+++ client/src/queries/combined-forecast-v2-query.ts
@@ -0,0 +1,25 @@
+import gql from "graphql-tag";
+
+export const COMBINED_FORECAST_V2_QUERY = gql`
+  query CombinedForecastV2($locationId: ID!) {
+    location(id: $locationId) {
+      combinedForecastV2 {
+        ...CombinedForecastV2Detail
+      }
+    }
+  }
+
+  fragment CombinedForecastV2Detail on CombinedForecastV2 {
+    name
+    date
+    wind {
+      timestamp
+      base
+      gusts
+      direction {
+        text
+        degrees
+      }
+    }
+  }
+`;
